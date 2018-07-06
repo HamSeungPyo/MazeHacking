19c5f44441607465d654be4d6db771969b8baea0
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2018-07-06 22:10:16 +0900

    캐릭터 샘플 작업중(1.0)

--- Assets/Script/CharacterControl.cs
+++ Assets/Script/CharacterControl.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterControl : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class CharacterControl : MonoBehaviour
+{
+	void Start ()
+    {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        int[] test = new int[2];
+        if (Input.GetKey(KeyCode.W))
+        {
+            test[1] = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            test[1] = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            test[0] = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            test[0] = 1;
+        }
+        transform.Translate(test[0], 0, test[1]);
+    }
 }
---
+++ Assets/Script/MoveJoystickControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MoveJoystickControl : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image bgImg;
+    private Vector2 inputVector;
+    void Start()
+    {
+        bgImg = GetComponent<Image>();
+    }
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        JoystickControl(ped);
+
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        JoystickControl(ped);
+        inputVector = Vector2.zero;
+    }
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        JoystickControl(ped);
+    }
+    void JoystickControl(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+            inputVector = new Vector2(pos.x, pos.y);
+            /*inputVector = new Vector2
+                (Mathf.Clamp(inputVector.x, -1f, 1f), Mathf.Clamp(inputVector.y, -1f, 1f));*/
+        }
+    }
+    public Vector2 GetAngleValue()
+    {
+        return inputVector;
+    }
+}
